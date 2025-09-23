33f9e099dfb2c88bae3002e1bffa05933fc95152
PieInOblivion <14125393+PieInOblivion@users.noreply.github.com>
2025-09-23 20:34:43 +1000

    change to local only proto usage

--- build.rs
+++ build.rs
@@ -1,33 +1,16 @@
-use std::path::Path;
-use std::process::Command;
-use std::{env, fs};
+use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR")?;
-    let out_dir_proto_path = Path::new(&out_dir).join("onnx.proto");
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let generated_rs = out_dir.join("onnx.rs");
 
-    // download onnx.proto if it doesn't exist in OUT_DIR
-    if !out_dir_proto_path.exists() {
-        let curl_res = Command::new("curl")
-            .arg("-L") // follow redirects
-            .arg("-s") // silent
-            .arg("-f") // fail on http errors
-            .arg("-o")
-            .arg(&out_dir_proto_path)
-            .arg("https://raw.githubusercontent.com/onnx/onnx/main/onnx/onnx.proto")
-            .output()?;
+    if !generated_rs.exists() {
+        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+        let repo_proto = manifest_dir.join("proto").join("onnx.proto");
 
-        // if curl failed, use local version
-        if !curl_res.status.success() {
-            let manifest = env::var("CARGO_MANIFEST_DIR")?;
-            let repo_proto = Path::new(&manifest).join("proto/onnx.proto");
-            fs::copy(&repo_proto, &out_dir_proto_path)?;
-        }
-
-        // compile the protobuf
         prost_build::Config::new()
             .bytes(["."])
-            .compile_protos(&[&out_dir_proto_path], &[&out_dir])?;
+            .compile_protos(&[&repo_proto], &[&manifest_dir])?;
     }
 
     Ok(())
